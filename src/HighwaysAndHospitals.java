2e56f3a179d437a73fdadb8d971b1042cc3fab04
kate <kate.little@menloschool.org>
2024-09-11 11:25:18 -0700

    9/11 - pseudocode

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
+
 /**
  * Highways & Hospitals
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Kate Little
  *
  */
 
@@ -15,6 +17,32 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+        long totalCost = 0;
+        ArrayList<ArrayList<Integer>> numClusters = new ArrayList<ArrayList<Integer>>();
+        // When building a hospital in every city is cheaper than building any highways
+        if (hospitalCost <= highwayCost){
+            // What does n represent?
+            totalCost = hospitalCost * n;
+        }
+        // When highway cost < hospital cost
+        else{
+            for (int[] city : cities){
+                for (ArrayList<Integer> cluster : numClusters){
+
+                }
+
+                // Check to see if either value 0 or 1 is already in any of numCluster's arraylists
+                // If no, make a new arraylist in numClusters & add both city[0] and city[1] there
+                // If yes, add the other value (not already in list) to that list
+                System.out.println(city[0] + " and " + city[1]);
+            }
+            // Need to separate all nodes into # of clusters
+            // Once I have # of clusters, cost of each cluster = cost of 1 hospital + cost of minimum # of highways
+        }
         return 0;
     }
+
+
+
+
 }
