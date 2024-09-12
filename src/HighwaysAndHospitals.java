228f8a4651cba2fb7976ef6d1fad147522a9e11c
kate <kate.little@menloschool.org>
2024-09-12 06:43:05 -0700

    9/12 - draft 1 done

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -18,28 +18,57 @@ public class HighwaysAndHospitals {
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
         long totalCost = 0;
-        ArrayList<ArrayList<Integer>> numClusters = new ArrayList<ArrayList<Integer>>();
-        // When building a hospital in every city is cheaper than building any highways
+        ArrayList<ArrayList<Integer>> cityClusters = new ArrayList<ArrayList<Integer>>();
+        // Case when building a hospital in every city is cheaper than building any highways
         if (hospitalCost <= highwayCost){
-            // What does n represent?
+            // What does n represent? I'm assuming it's the number of cities
             totalCost = hospitalCost * n;
         }
-        // When highway cost < hospital cost
+        // Case when highway cost < hospital cost
         else{
+            // Go through every city
             for (int[] city : cities){
-                for (ArrayList<Integer> cluster : numClusters){
-
+                //
+                boolean addedToCluster = false;
+                for (ArrayList<Integer> cluster : cityClusters){
+                    // If it contains 1 of the 2 listed, cities, add the other
+                    if (cluster.contains(city[0]) && !cluster.contains(city[1])){
+                        cluster.add(city[1]);
+                        addedToCluster = true;
+                        break;
+                    }
+                    else if (cluster.contains(city[1]) && !cluster.contains(city[0])){
+                        cluster.add(city[0]);
+                        addedToCluster = true;
+                        break;
+                    }
+                    // If both have already been added to the cluster
+                    else if (cluster.contains(city[1]) && cluster.contains(city[0])){
+                        addedToCluster = true;
+                    }
                 }
-
-                // Check to see if either value 0 or 1 is already in any of numCluster's arraylists
-                // If no, make a new arraylist in numClusters & add both city[0] and city[1] there
-                // If yes, add the other value (not already in list) to that list
-                System.out.println(city[0] + " and " + city[1]);
+                // If none of the existing clusters contain either city, make a new cluster & add both cities there
+                if (!addedToCluster){
+                    ArrayList<Integer> newCluster = new ArrayList<Integer>();
+                    newCluster.add(city[0]);
+                    newCluster.add(city[1]);
+                    cityClusters.add(newCluster);
+                }
             }
-            // Need to separate all nodes into # of clusters
-            // Once I have # of clusters, cost of each cluster = cost of 1 hospital + cost of minimum # of highways
+            // TEMPORARILY prints out all clusters for test
+            for (ArrayList<Integer> cluster : cityClusters){
+                System.out.println(cluster);
+            }
+
+            // Calculates cost
+            int numClusters = cityClusters.size();
+            totalCost = hospitalCost * numClusters;
+            for (ArrayList<Integer> cluster : cityClusters){
+                // Adds the cost of highways per cluster;
+                totalCost += (cluster.size() - 1)*highwayCost;
+            }
         }
-        return 0;
+        return totalCost;
     }
 
 
