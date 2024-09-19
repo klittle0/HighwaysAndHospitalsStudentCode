bec8c5c77500586d34193972e34805b39200ae62
kate <kate.little@menloschool.org>
2024-09-19 10:35:12 -0700

    9/19 - fixed infinite loop

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -19,82 +19,80 @@ public class HighwaysAndHospitals {
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
         long totalCost = 0;
         // List to track each city & its root
-        int[] roots = new int[n];
+        int[] roots = new int[n + 1];
         // Case when building a hospital in every city is cheaper than building any highways
         if (hospitalCost <= highwayCost){
-            // What does n represent? I'm assuming it's the number of cities
-            totalCost = hospitalCost * n;
+            return hospitalCost * n;
         }
         // Case when highway cost < hospital cost
-        else{
-            // Perform union find to get the right # of clusters
 
-            // If I set them to 0, will this cause issues??
-            int rootOne = 0;
-            int rootTwo = 0;
-            for (int[] city : cities){
-                rootOne = city[0];
-                // while the city is not its own root
-                while (roots[city[0]] != city[0]){
-                    // Identify root for city
-                    rootOne = roots[city[0]];
-                }
-                // While the city is not its own root
-                while (roots[city[1]] != city[1]){
-                    rootTwo = roots[city[1]];
-                    // Path Compression here:
-                    int temp = rootTwo;
-                    roots[city[1]] = temp;
-
-
-
-                }
-                if (rootOne != rootTwo){
-                    // Weight balancing here:
-                    // Find the order of both roots
-                    int orderOne = roots[rootOne];
-                    int orderTwo = roots[rootTwo];
-                    // Set the root of the bigger tree as the root of the smaller tree
-                    if (orderOne > orderTwo){
-                        // Instead of roots[rootOne], could I just say orderOne??
-                        roots[rootTwo] = roots[rootOne];
-                    }
-
-                }
-                // After weight balancing, then add the proper values to the roots[] array
-
+        // Perform union find to get the right # of clusters
+        for (int[] city : cities){
+            int cityOne = city[0];
+            int cityOneCopy = city[0];
+            int cityTwo = city[1];
+            int cityTwoCopy= city[1];
+            // while the city is not its own root, aka while its root is positive
+            while (roots[cityOne] > 0){
+                // Identify root for city
+                cityOne = roots[cityOne];
             }
-
-
-
-            // Go through every city
-            for (int i = 0; i < n; i++){
-                // Get the root of each city in pair
-                int root1 = cityClusters[cities[i][0]];
-                int root2 = cityClusters[cities[i][1]];
-                // if the roots are equal, do nothing. If they're not equal, set the topmost root = root1
-                if (root1 != root2){
-
+            System.out.println("copy: " + cityOneCopy);
+            System.out.print("current" + cityOne);
+            // Path compression here:
+            // Make the top root the root for all lower nodes
+            while (roots[cityOneCopy] > 0){
+                // Set the new root for each city that we traversed previously
+                int tempRoot = roots[cityOneCopy];
+                roots[cityOneCopy] = cityOne;
+                cityOneCopy = tempRoot;
+            }
+            // While the city is not its own root, aka while its root is positive
+            while (roots[cityTwo] > 0){
+                // Identify root
+                cityTwo = roots[cityTwo];
+            }
+            // Path compression here:
+            // Make the top root the root for all lower nodes
+            while (roots[cityTwoCopy] > 0){
+                // Set the new root for each city that we traversed previously
+                int tempRoot = roots[cityTwoCopy];
+                roots[cityTwoCopy] = cityTwo;
+                cityTwoCopy = tempRoot;
+            }
+            if (cityOne != cityTwo){
+                // Weight balancing here:
+                // Find the order of both roots
+                int orderOne = roots[cityOne];
+                int orderTwo = roots[cityTwo];
+                // Set the root of the bigger tree as the root of the smaller tree
+                if (orderOne < orderTwo){
+                    // Updates the order of root 1 & root of root 2
+                    roots[cityOne] += (orderTwo - 1);
+                    roots[cityTwo] = cityOne;
                 }
-
-            }
-            // TEMPORARILY prints out all clusters for test
-            for (ArrayList<Integer> cluster : cityClusters){
-                System.out.println(cluster);
-            }
-
-            // Calculates cost
-            int numClusters = cityClusters.size();
-            totalCost = hospitalCost * numClusters;
-            for (ArrayList<Integer> cluster : cityClusters){
-                // Adds the cost of highways per cluster;
-                totalCost += (cluster.size() - 1)*highwayCost;
+                else if (orderOne > orderTwo){
+                    // Updates the order of root 2 & root of root 1
+                    roots[cityTwo] += (orderOne - 1);
+                    roots[cityOne] = cityTwo;
+                }
             }
         }
+
+
+            // find # of clusters:
+            int numClusters = 0;
+
+            for (int root : roots){
+                // Since the roots with themselves as a root hold a negative value
+                if (root < 0){
+                    numClusters += 1;
+                }
+            }
+            // Calculate cost
+            totalCost += hospitalCost * numClusters + (n - numClusters)*highwayCost;
+
         return totalCost;
     }
-
-
-
 
 }
